ef15deccc779226f468975af7c5a92b7304806a9
shafira25001 <shafira25001@mail.unpad.ac.id>
2025-11-11 20:40:38 +0700

    Add files via upload

---
+++ surat.cpp
@@ -0,0 +1,39 @@
+/*
+Nama Program :
+Nama         :
+NPM          :
+Tanggal Buat :
+Deskripsi    :
+*/
+
+#include<iostream>
+using namespace std;
+
+struct Surat {
+    string penerima;
+    string pengirim;
+    string nomorTelepon;
+};
+
+Surat inputData(){
+    Surat temp;
+    cout << "masukkan nama penerima surat: ";
+    getline(cin, temp.penerima);
+    cout << "masukkan nama pengirim surat: ";
+    getline(cin, temp.pengirim);
+    cout << "masukkan nomor telepon penerima surat: ";
+    getline(cin, temp.nomorTelepon);
+    return temp;
+}
+
+void tampilkanData(const Surat &surat){
+    cout << "nama penerima : " << surat.penerima << endl;
+    cout << "nama pengirim : " << surat.pengirim << endl;
+    cout << "nomor telepon penerima surat: " << surat.nomorTelepon << endl;
+}
+
+int main(){ 
+    Surat temp = inputData();
+    tampilkanData(temp);
+
+}
