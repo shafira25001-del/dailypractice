0b8e5b95a7d9863caf8e8caa1a97d9122d9fb9ec
shafira25001 <shafira25001@mail.unpad.ac.id>
2025-11-26 21:36:45 +0700

    Add files via upload

---
+++ nilai.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+using namespace std;
+int main(){
+/*
+    int nilai;
+    cout << "masukkan nilai anda: ";
+    cin >> nilai;
+
+    if(nilai >80){
+        cout << "IP anda A";
+    } else if (nilai >=65 && nilai <80){
+        cout << "IP anda B";
+    } else if (nilai >= 50 && nilai <65){
+        cout << "IP anda C";
+    } else {
+        "IP anda E";
+    }
+
+    return 0;
+
+}
+*\
+
